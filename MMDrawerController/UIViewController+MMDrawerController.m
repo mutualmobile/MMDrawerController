dc2cf23133e0967b3535e23c39ad08be9e7c70ab
Kevin Wong <snarpel@gmail.com>
2014-09-08 15:05:22 -0600

    Calculate the visible drawer frames based on the center view controller to account for the dummy status bar view.

--- MMDrawerController/UIViewController+MMDrawerController.m
+++ MMDrawerController/UIViewController+MMDrawerController.m
@@ -38,14 +38,14 @@
 -(CGRect)mm_visibleDrawerFrame{
     if([self isEqual:self.mm_drawerController.leftDrawerViewController] ||
        [self.navigationController isEqual:self.mm_drawerController.leftDrawerViewController]){
-        CGRect rect = self.mm_drawerController.view.bounds;
+        CGRect rect = self.mm_drawerController.centerViewController.view.bounds;
         rect.size.width = self.mm_drawerController.maximumLeftDrawerWidth;
         return rect;
         
     }
     else if([self isEqual:self.mm_drawerController.rightDrawerViewController] ||
              [self.navigationController isEqual:self.mm_drawerController.rightDrawerViewController]){
-        CGRect rect = self.mm_drawerController.view.bounds;
+        CGRect rect = self.mm_drawerController.centerViewController.view.bounds;
         rect.size.width = self.mm_drawerController.maximumRightDrawerWidth;
         rect.origin.x = CGRectGetWidth(self.mm_drawerController.view.bounds)-rect.size.width;
         return rect;
