607b371c6c4de3051735ae18d9361e7c28580c32
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-03-11 15:11:16 -0500

    Fix for #287

--- MMDrawerController/UIViewController+MMDrawerController.m
+++ MMDrawerController/UIViewController+MMDrawerController.m
@@ -40,6 +40,9 @@
        [self.navigationController isEqual:self.mm_drawerController.leftDrawerViewController]){
         CGRect rect = self.mm_drawerController.view.bounds;
         rect.size.width = self.mm_drawerController.maximumLeftDrawerWidth;
+        if (self.mm_drawerController.showsStatusBarBackgroundView) {
+            rect.size.height -= 20.0f;
+        }
         return rect;
         
     }
@@ -48,6 +51,9 @@
         CGRect rect = self.mm_drawerController.view.bounds;
         rect.size.width = self.mm_drawerController.maximumRightDrawerWidth;
         rect.origin.x = CGRectGetWidth(self.mm_drawerController.view.bounds)-rect.size.width;
+        if (self.mm_drawerController.showsStatusBarBackgroundView) {
+            rect.size.height -= 20.0f;
+        }
         return rect;
     }
     else {
