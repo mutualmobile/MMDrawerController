84f5ce7644adf060f60426fe571be6ccae4d7d3b
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-03-16 10:03:29 -0500

    Switched id to instancetype

--- MMDrawerController/MMDrawerBarButtonItem.h
+++ MMDrawerController/MMDrawerBarButtonItem.h
@@ -38,7 +38,7 @@
  
  @return The newly-initialized bar button item.
  */
--(id)initWithTarget:(id)target action:(SEL)action;
+-(instancetype)initWithTarget:(id)target action:(SEL)action;
 
 /**
  Returns the current color of the menu button for the state requested. This property is deprecated in iOS 7.0. Use `tintColor` instead.
